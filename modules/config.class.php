b3aa27f354765c8ffde2850e0b09557c7dce3e77
Moritz Kaspar Rudert (mortzu) <me@mortzu.de>
2010-04-13 15:46:17 +0000

    config list

--- modules/config.class.php
+++ modules/config.class.php
@@ -9,15 +9,16 @@ class config {
 		global $logdir;
 		global $room_topic;
 		global $rooms_log;
+		global $config;
 
 		$i = 0;
 
-		while($timestamp == "" && $i < 5) {
+		while ($timestamp == "" && $i < 5) {
 			$timestamp = strtotime($message["message"]["#"]["x"][$i]["@"]["stamp"]);
 			$i++;
 		}
 
-		if($timestamp)
+		if ($timestamp)
 			return;
 
 		$from = $JABBER->GetInfoFromMessageFrom($message);
@@ -25,42 +26,58 @@ class config {
 		$from = $from_temp[0];
 		$msg = $JABBER->GetInfoFromMessageBody($message);
 		$user = $from_temp[1];
-		
-		if(!in_array($from, $trust_users))
+
+		if (!in_array($from, $trust_users))
 			return;
 
-		if(preg_match("/^config (set|get|del) (.*)$/i", $msg, $matches)) {
+		if (preg_match("/^config (set|get|del|list) (.*)$/i", $msg, $matches)) {
 			$return = "FAIL!";
 
-			if($matches[1] == "set" && preg_match("/^([^:]{1,}):(.*)$/i", $matches[2], $submatches)) {
+			if ($matches[1] == "set" && preg_match("/^([^:]{1,}):(.*)$/i", $matches[2], $submatches)) {
 				set_config($submatches[1], $submatches[2]);
 
-				if(get_config($submatches[1]) == $submatches[2])
+				if (get_config($submatches[1]) == $submatches[2])
 					$return = "ok.";
 				else
 					$return = "nicht ok.\n\ndas heisst FAIL!";
-			} elseif($matches[1] == "get") {
+			}
+			elseif ($matches[1] == "get") {
 				$return = utf8_decode(get_config($matches[2]));
 				$return = utf8_encode($return);
 
-				if($return == "")
+				if ($return == "")
 					$return = "nix is da gewesen.";
-			} elseif($matches[1] == "del") {
+			}
+			elseif ($matches[1] == "del") {
 				del_config($matches[2]);
-				if(get_config($matches[2]) == "")
+				if (get_config($matches[2]) == "")
 					$return = "deleted";
 			}
 
-			$JABBER->SendMessage($from, "chat", NULL, array("body" => $return));
-		} elseif(preg_match("/^config rehash$/i", $msg)) {
-			global $config;
-			$config = array();
-			$JABBER->SendMessage($from, "chat", NULL, array("body" => "ok."));
+			$JABBER->SendMessage($from, "chat", NULL, array (
+				"body" => $return
+			));
+		}
+		elseif (preg_match("/^config rehash$/i", $msg)) {
+			$config = array ();
+			$JABBER->SendMessage($from, "chat", NULL, array (
+				"body" => "ok."
+			));
+		}
+		elseif (preg_match("/^config list$/i", $msg)) {
+			$return = "actual config:\n";
+			$result = make_mysql_query("SELECT * FROM `config` ORDER BY `name`;");
+			while ($row = make_mysql_fetch_array($result, MYSQL_ASSOC)) {
+				$return .= $row['name'] . ": " . $row['value'] . "\n";
+			}
+			$JABBER->SendMessage($from, "chat", NULL, array (
+				"body" => $return
+			));
 		}
 	}
 
 	public static function trustHelp() {
-		return "config set foo:bar|get foo|del foo|rehash to clear cache";
+		return "config set foo:bar|get foo|del foo|rehash to clear cache|list";
 
 	}
 

4d9b74e7732d812b9eb9b5136018ab6a5f935b53
Jan-Philipp Litza <mail@deadorc.de>
2011-09-12 20:33:44 +0200

    RSS: outsource (un)subscription to methods, enable admin commands

--- modules/rss.class.php
+++ modules/rss.class.php
@@ -3,6 +3,11 @@ require_once "extlib/simplepie/simplepie.inc";
 
 class rss {
 
+	const ERROR_GENERIC = 1;
+	const ERROR_ALREADY_SUBSCRIBED = 2;
+	const ERROR_NOT_SUBSCRIBED = 3;
+	const ERROR_NOT_A_FEED = 4;
+
 	public static function cron($i) {
 		global $JABBER;
 		global $check_hosts;
@@ -103,52 +108,52 @@ class rss {
 		$msg = $JABBER->GetInfoFromMessageBody($message);
 		$user = $from_temp[1];
 
-		if (preg_match("#^subscribe https?://#", $msg)) {
-			list(, $url) = explode(' ', $msg, 2);
+		if (preg_match("#^subscribe(?: ([^@\s]+@[^@\s]+))? (https?://.*)$#", $msg, $match)) {
+			list($jid, $url) = $match;
+			if (!empty($jid) && !in_array($from, $trust_users))
+				return;
 
-			$result = make_sql_num_query("SELECT * FROM `rss_subscriptions` WHERE `rss_url` = '" . make_sql_escape($url) . "' AND `jid` = '" . make_sql_escape($from) . "';");
-			if ($result > 0)
-				$msg = "You are already subscribed to " . $url;
-			else {
-				$feed = new SimplePie();
-				$feed->set_feed_url($url);
-				$feed->enable_cache(false);
-				if ($feed->init()) {
-					$feed->handle_content_type();
-					if($feed->get_type() & SIMPLEPIE_TYPE_ALL) {
-						# feed is valid and supported
-						make_sql_query("INSERT INTO `rss_subscriptions` (`rss_url`, `jid`) VALUES ('" . make_sql_escape($url) . "', '" . make_sql_escape($from) . "');");
-						if(make_sql_affected_rows() == 1)
-							$msg = "You have been successfully subscribed to " . $url . ". To unsubscribe, send me\nunsubscribe " . $url;
-						else
-							$msg = "Sorry, subscribing you to " . $url . " failed.";
-					}
-					else
-						$msg = $url . " is not a valid feed!";
-				}
-				else
-					$msg = "Errors retrieving " . $url;
+			if (empty($jid))
+				$jid = $from;
+
+			switch(self::subscribe($jid, $url)) {
+				case 0:
+					$msg = "Subscription successful. To unsubscribe, send me \"unsubscribe " . (($jid != $from)? $jid . " " : "") . $url . "\"";
+					break;
+				case self::ERROR_ALREADY_SUBSCRIBED:
+					$msg = "Subscription failed: " . $jid . " are already subscribed to " . $url;
+					break;
+				case self::ERROR_NOT_A_FEED:
+					$msg = "Subscription failed: " . $url . " is not a valid feed.";
+					break;
+				default:
+					$msg = "Subscription failed.";
+					break;
 			}
 
 			$JABBER->SendMessage($from, "chat", NULL, array (
 				"body" => $msg
 			));
 		}
-		if (preg_match("#^unsubscribe https?://#", $msg)) {
-			list(, $url) = explode(' ', $msg, 2);
-			$subscribed = make_sql_num_query("SELECT * FROM `rss_subscriptions` WHERE `rss_url` = '" . make_sql_escape($url) . "' AND `jid` = '" . make_sql_escape($from) . "');");
-			if ($subscribed) {
-				make_sql_query("DELETE FROM `rss_subscriptions` WHERE `rss_url` = '" . make_sql_escape($url) . "' AND `jid` = '" . make_sql_escape($from) . "');");
-				if(make_sql_affected_rows() == 1)
-					$msg = "You have been successfully unsubscribed from " . $url;
-				else
-					$msg = "Sorry, unsubscribing you from " . $url . " failed.";
+		elseif (preg_match("#^unsubscribe(?: ([^@\s]+@[^@\s]+))? (https?://.*)$#", $msg, $match)) {
+			list($jid, $url) = $match;
+			if (!empty($jid) && !in_array($from, $trust_users))
+				return;
 
-				# if this was the last subscriber, delete the cached entries
-				if(make_sql_num_query("SELECT * FROM `rss_subscriptions` WHERE `rss_url` = '" . make_sql_escape($url) . "';") === 0)
-					make_sql_query("DELETE FROM `rss_feeds` WHERE `rss_url` = '" . make_sql_escape($url) . "';");
-			} else
-				$msg = "You are not subscribed to " . $url . ", thus I can't unsubscribe you.";
+			if (empty($jid))
+				$jid = $from;
+
+			switch(self::unsubscribe($from, $url)) {
+				case 0:
+					$msg = "Unsubscription successful.";
+					break;
+				case self::ERROR_NOT_SUBSCRIBED:
+					$msg = "Unsubscription failed: You are not subscribed to " . $url;
+					break;
+				default:
+					$msg = "Unsubscription failed.";
+					break;
+			}
 
 			$JABBER->SendMessage($from, "chat", NULL, array (
 				"body" => $msg
@@ -156,8 +161,52 @@ class rss {
 		}
 	}
 
+	private static function subscribe($jid, $url) {
+		$result = make_sql_num_query("SELECT * FROM `rss_subscriptions` WHERE `rss_url` = '" . make_sql_escape($url) . "' AND `jid` = '" . make_sql_escape($jid) . "';");
+		if ($result > 0)
+			return self::ERROR_ALREADY_SUBSCRIBED;
+
+		$feed = new SimplePie();
+		$feed->set_feed_url($url);
+		$feed->enable_cache(false);
+		if ($feed->init()) {
+			$feed->handle_content_type();
+			if($feed->get_type() & SIMPLEPIE_TYPE_ALL) {
+				# feed is valid and supported
+				make_sql_query("INSERT INTO `rss_subscriptions` (`rss_url`, `jid`) VALUES ('" . make_sql_escape($url) . "', '" . make_sql_escape($jid) . "');");
+				if(make_sql_affected_rows() != 1)
+					return self::ERROR_GENERIC;
+			}
+			else
+				return self::ERROR_NOT_A_FEED;
+		}
+		else
+			return self::ERROR_NOT_A_FEED;
+		return 0;
+	}
+
+	private static function unsubscribe($jid, $url) {
+		$subscribed = make_sql_num_query("SELECT * FROM `rss_subscriptions` WHERE `rss_url` = '" . make_sql_escape($url) . "' AND `jid` = '" . make_sql_escape($jid) . "';");
+		if (!$subscribed)
+			return self::ERROR_NOT_SUBSCRIBED;
+
+		make_sql_query("DELETE FROM `rss_subscriptions` WHERE `rss_url` = '" . make_sql_escape($url) . "' AND `jid` = '" . make_sql_escape($jid) . "';");
+		if(make_sql_affected_rows() != 1)
+			return self::ERROR_GENERIC;
+
+		# if this was the last subscriber, delete the cached entries
+		if(make_sql_num_query("SELECT * FROM `rss_subscriptions` WHERE `rss_url` = '" . make_sql_escape($url) . "';") === 0)
+			make_sql_query("DELETE FROM `rss_feeds` WHERE `rss_url` = '" . make_sql_escape($url) . "';");
+		
+		return 0;
+	}
+
 	public static function help() {
-		return "subscribe <url> - subscribe to the feed <url>";
+		return "subscribe <url> - subscribe you to the feed <url>";
+	}
+
+	public static function trustHelp() {
+		return "subscribe <jid> <url> - subscribe <jid> to the feed <url>";
 	}
 }
 ?>

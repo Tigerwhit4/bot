54432016ab30ac82e5b8f7b8c2d53887ee2b3bde
Jan-Philipp Litza <mail@deadorc.de>
2011-09-12 20:43:06 +0200

    credits: added jplitza (me) for adding RSS stuff

---
+++ modules/credits.class.php
@@ -0,0 +1,49 @@
+<?php
+class credits {
+	public static function groupchat($message) {
+		global $JABBER;
+		global $check_hosts;
+		global $trusted_users;
+		global $trust_users;
+		global $logdir;
+		global $room_topic;
+		global $rooms_log;
+		global $rooms;
+
+		$i = 0;
+
+		while($timestamp == "" && $i < 5) {
+			$timestamp = strtotime($message["message"]["#"]["x"][$i]["@"]["stamp"]);
+			$i++;
+		}
+
+		if($timestamp)
+			return;
+
+		$from = $JABBER->GetInfoFromMessageFrom($message);
+		$from_temp = explode("/", $from);
+		$from = $from_temp[0];
+		$msg = $JABBER->GetInfoFromMessageBody($message);
+		$user = $from_temp[1];
+
+		if($JABBER->username == $user)
+			return;
+
+		if($msg == "!credits") {
+				$msg = "my founder was helios <helios@planetcyborg.de>\n";
+				$msg .= "msquare added a lot of code and therefor he is my co-founder\n";
+				$msg .= "---\n";
+				$msg .= "helios: !mensa, !gw2, !hsmensa, !topic, !stream, !moo, !addquote, !quote, !gbo, !bash, !ddate, !pi, !date, !number, !checkaps, !fortune, !youporn\n";
+				$msg .= "jix: !google, !wikipedia\n";
+				$msg .= "msquare: !ticket, !tabu, !wetter, !revue, !rechner\n";
+
+				$JABBER->SendMessage($from, "groupchat", NULL, array("body" => trim($msg)));
+			}
+	}
+
+	public static function help() {
+		return "!credits - shows my credits";
+	}
+
+}
+?>

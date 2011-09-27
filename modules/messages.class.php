055ff7e14ca9ec50d7253c455573df9bf4b04795
Jan-Philipp Litza <janphilipp@litza.de>
2011-09-27 16:44:49 +0200

    first attempt of a network messages module

--- modules/messages.class.php
+++ modules/messages.class.php
@@ -1,6 +1,7 @@
 <?php
 class messages {
 	private static $thread;
+	private static $port = 59152;
 
 	public static function init() {
 		self::$thread = new Thread(array(__CLASS__, 'worker'));
@@ -11,19 +12,37 @@ class messages {
 		global $JABBER;
 
 		while (true) {
-			if(file_exists("/var/lib/yoda/message_queue")) {
-				$msgs = file("/var/lib/yoda/message_queue");
+			if(!($socket = socket_create(AF_INET6, SOCK_STREAM, SOL_TCP)))
+				die('socket_create() failed: ' . socket_strerror(socket_last_error()));
+			if(!socket_bind($socket, 0, self::$port))
+				die('socket_bind() failed: ' . socket_strerror(socket_last_error($socket)));
+			if(!socket_listen($socket))
+				die('socket_listen() failed: ' . socket_strerror(socket_last_error($socket)));
 
-				foreach($msgs as $msg) {
-					preg_match('/^"(.*?)" "(.*?)"(?: "(.*)")?$/i', $msg, $matches);
-					if(preg_match('/^[0-9a-zA-Z_-]*@[0-9a-zA-Z_.-]*$/i', $matches[1])) {
-						if(isset($matches[3]) && $matches[3] == "muc")
-							$JABBER->SendMessage($matches[1], "groupchat", NULL, array("body" => rtrim(utf8_encode($matches[2]))));
-						else
-							$JABBER->SendMessage($matches[1], "chat", NULL, array("body" => rtrim(utf8_encode($matches[2]))));
+			while ($client = socket_accept($socket)) {
+				$clientaddress = "";
+				socket_getpeername($socket, $clientaddress);
+				$token = socket_read($client, 128, PHP_NORMAL_READ);
+				$tokens = json_decode(get_config('message_tokens'));
+
+				if ($token == $tokens[$clientaddress]) {
+					while($msg = socket_read($client, 1024, PHP_NORMAL_READ) && !empty($msg)) {
+						preg_match('/^"(.*?)" "(.*?)"(?: "(.*)")?$/i', $msg, $matches);
+						if(preg_match('/^[0-9a-zA-Z_-]*@[0-9a-zA-Z_.-]*$/i', $matches[1])) {
+							if(isset($matches[3]) && $matches[3] == "muc")
+								$JABBER->SendMessage($matches[1], "groupchat", NULL, array("body" => rtrim(utf8_encode($matches[2]))));
+							else
+								$JABBER->SendMessage($matches[1], "chat", NULL, array("body" => rtrim(utf8_encode($matches[2]))));
+						}
 					}
 				}
+
+				socket_shutdown($client, 2);
+				socket_close($client);
 			}
+
+			socket_shutdown($socket, 2);
+			socket_close($socket);
 		}
 		die();
 	}

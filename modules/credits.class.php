719f2b50b0155dece199a8f9c514a7bf1ad687f5
Moritz Kaspar Rudert (mortzu) <me@mortzu.de>
2013-12-15 11:07:09 +0100

    s/helios/mortzu/

--- modules/credits.class.php
+++ modules/credits.class.php
@@ -2,10 +2,10 @@
 class credits {
   public static function groupchat($message, $from, $user, $msg) {
     if($msg == "!credits") {
-        $answer = "my founder was helios <helios@planetcyborg.de>\n";
+        $answer = "my founder was mortzu <me@mortzu.de>\n";
         $answer .= "msquare added a lot of code and therefor he is my co-founder\n";
         $answer .= "---\n";
-        $answer .= "helios: !mensa, !gw2, !hsmensa, !topic, !stream, !moo, !addquote, !quote, !gbo, !bash, !ddate, !pi, !date, !number, !checkaps, !fortune, !youporn\n";
+        $answer .= "mortzu: !mensa, !gw2, !hsmensa, !topic, !stream, !moo, !addquote, !quote, !gbo, !bash, !ddate, !pi, !date, !number, !checkaps, !fortune, !youporn\n";
         $answer .= "msquare: !ticket, !tabu, !wetter, !revue, !rechner\n";
         $answer .= "jplitza: RSS subscription\n";
 

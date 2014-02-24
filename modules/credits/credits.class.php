fbfffa79f34a55a13dd30188c97eb0a97da1c031
Moritz Kaspar Rudert (mortzu) <me@mortzu.de>
2014-02-24 17:51:49 +0100

    simplified credits

--- modules/credits/credits.class.php
+++ modules/credits/credits.class.php
@@ -2,12 +2,7 @@
 class credits {
   public static function groupchat($message, $from, $user, $msg) {
     if($msg == "!credits") {
-        $answer = "my founder was mortzu <me@mortzu.de>\n";
-        $answer .= "msquare added a lot of code and therefor he is my co-founder\n";
-        $answer .= "---\n";
-        $answer .= "mortzu: !mensa, !gw2, !hsmensa, !topic, !stream, !moo, !addquote, !quote, !gbo, !bash, !ddate, !pi, !date, !number, !checkaps, !fortune, !youporn\n";
-        $answer .= "msquare: !ticket, !tabu, !wetter, !revue, !rechner\n";
-        $answer .= "jplitza: RSS subscription\n";
+        $answer = "Jabberbotframework: https://gitlab.planetcyborg.de/planetcyborg/yoda\n";
 
         return $answer;
       }
